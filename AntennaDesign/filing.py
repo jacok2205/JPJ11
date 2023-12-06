9a2e6e66f45ea419ec0ea099979d5d05442e960d
Jakes-Work <jacok2205@gmail.com>
2023-12-06 19:22:29 +0200

    Completed code reduction, documentation, and neatness.
    Testing is required in order to verify functionality of the class.

--- AntennaDesign/filing.py
+++ AntennaDesign/filing.py
@@ -9,8 +9,8 @@
 
     Imports:
     --------
-    AntennaDesign.__init__:     The initialization module for the package, which will be a list of common public
-                                libraries.
+    AntennaDesign.__init__:         The initialization module for the package, which will be a list of common public
+                                    libraries.
 
     Notes:
     ------
@@ -29,10 +29,10 @@ class Filing:
 
         Parameters:
         -----------
-        Directories:        list
-                            A list of string elements that represent the directories that will be created.
-        Debugging:          bool
-                            For debugging purposes (developer mode).
+        Directories:                list
+                                    A list of string elements that represent the directories that will be created.
+        Debugging:                  bool
+                                    For debugging purposes (developer mode).
 
         Returns:
         --------
@@ -43,10 +43,12 @@ class Filing:
         None.
         """
 
+        # Create an absolute path string with the 'Filing' directory that potentially does not yet exist
         self.__directory__ = str(pkg_resources.files('AntennaDesign') / 'Filing')
+        # For debugging (developer mode)
         self.__debugging__ = Debugging
 
-        # Initialize/Create default directories (the root directory is Dataset)
+        # Initialize/Create default directories (the root directory is Filing)
         __temp__ = ''
         for __i__ in self.__directory__.split(sep='\\'):
             __temp__ += __i__ + '\\'
@@ -57,7 +59,7 @@ class Filing:
                     if self.__debugging__:
                         print(__error__)
 
-        # Create extra directories according to the defined Directories variable
+        # Create extra directories according to the defined Directories, if not None, variable
         if Directories is not None:
             for __i__ in Directories:
                 __temp__ = self.__directory__ + '\\'
@@ -71,8 +73,28 @@ class Filing:
                                 print(__error__)
 
     def CreateDirectories(self, Directories=None):
+        """"
+        Description:
+        ------------
+        Creates a tree of directories under the root directory defined in the constructor of this class.
+
+        Parameters:
+        -----------
+        Directories:                list
+                                    A list of str type elements that are used to create the directory(ies).
+
+        Returns:
+        --------
+        Returns 0 if all/most the directories received from Directories has been created, else -1 if
+        Directories is of None type.s
+
+        Notes:
+        ------
+        None.
+        """
+
         if Directories is None:
-            return
+            return -1
 
         for __i__ in Directories:
             __temp__ = self.__directory__
@@ -84,8 +106,29 @@ class Filing:
                     except Exception as __error__:
                         if self.__debugging__:
                             print(__error__)
+        return 0
 
     def CreateFile(self, Filename):
+        """"
+        Description:
+        ------------
+        Creates a file given the Filename parameter.
+
+        Parameters:
+        -----------
+        Filename:                   str
+                                    The name of the file to be created. Note that the extensions should be
+                                    specified if the user wishes so.
+
+        Returns:
+        --------
+        Returns 0 if the file was created successfully, else -1 if the file already exists.
+
+        Notes:
+        ------
+        None.
+        """
+
         if os.path.exists(path=self.__directory__ + Filename):
             return -1
         __temp__ = open(self.__directory__ + Filename, 'w')
@@ -93,6 +136,26 @@ class Filing:
         return 0
 
     def DeleteContent(self, Filename):
+        """"
+        Description:
+        ------------
+        Wipes the content/data within the specified file.
+
+        Parameters:
+        -----------
+        Filename:                   str
+                                    The name of the file for the data to be erased. If an extension to the file
+                                    exists, it must be included.
+
+        Returns:
+        --------
+        None.
+
+        Notes:
+        ------
+        If the file does not exist, the file will be created without the intention of doing so.
+        """
+
         __temp__ = open(self.__directory__ + Filename, 'w')
         __temp__.close()
 
@@ -101,7 +164,7 @@ class Filing:
         Description:
         ------------
         Attempts to read all the contents from the given filename, which should be the full path, including the
-        actual file and its extension, to read.
+        extension, to read.
 
         Parameters:
         -----------
@@ -109,9 +172,10 @@ class Filing:
                                     The full path, with the filename and possible extension, of the file
                                     to do the intended operation on.
 
-        Return:
+        Returns:
         -------
-        If the file does not exist, -1 will be returned, else a list that has been extracted from the file will be returned.
+        If the file does not exist, -1 will be returned, else a list that has been extracted from the file
+        will be returned.
 
         Notes:
         ------
@@ -155,21 +219,15 @@ class Filing:
         """
         Description:
         ------------
-        Attempts to append a single individual's data.
+        Attempts to append a list to a file with Filename.
 
         Parameters:
         -----------
-        __filename__:                   str
-                                        The full path, with the filename and possible extension, of the file
-                                        to do the intended operation on.
-        __candidate_number__:           int
-                                        The number to name the individual.
-        __gen__:                        list
-                                        The data of the individual.
-        __custom__:                     bool
-                                        When True, the __config__.__generation_number__ global variable is not used.
-                                        Conversely, if False, the __config__.__generation_number__ global variable is
-                                        used as a postfix to the __filename__ parameter.
+        Filename:                   str
+                                    The full path, with the filename and possible extension, of the file
+                                    to do the intended operation on.
+        List:                       list
+                                    A list to be appended onto the file Filename.
 
         Return:
         -------
@@ -177,7 +235,7 @@ class Filing:
 
         Notes:
         ------
-        This function is executed when generation_file() receives __mode__='append'.
+        None.
         """
 
         try:
@@ -198,16 +256,17 @@ class Filing:
         """
         Description:
         ------------
-        Attempts to save the full generation data.
+        Attempts to save all the contents in the Lists parameter given the filename.
 
         Parameters:
         -----------
-        __filename__:                   str
-                                        The full path, with the filename and possible extension, of the file
-                                        to do the intended operation on.
-        __gen__:                        list
-                                        A list of all the individuals in current or new generation population, where each
-                                        individual is a list.
+        Filename:                   str
+                                    The full path, with the filename and possible extension, of the file
+                                    to do the intended operation on.
+        Lists:                      list
+                                    A list of lists to save into the file given the Filename parameter. Note that
+                                    all the data within the file will be replaced with the content from the Lists
+                                    parameter.
 
         Return:
         -------
@@ -215,7 +274,7 @@ class Filing:
 
         Notes:
         ------
-        This function is executed when generation_file() receives __mode__='save'.
+        None.
         """
 
         try:
@@ -241,9 +300,9 @@ class Filing:
 
         Parameters:
         -----------
-        __filename__:                   str
-                                        The full path, with the filename and possible extension, of the file
-                                        to do the intended operation on.
+        Filename:                   str
+                                    The full path, with the filename and possible extension, of the file
+                                    to do the intended operation on.
 
         Return:
         -------
@@ -251,7 +310,7 @@ class Filing:
 
         Notes:
         ------
-        This function is executed when generation_file() receives __mode__='delete'.
+        None.
         """
 
         try:
@@ -269,27 +328,20 @@ class Filing:
         """
         Description:
         ------------
-        Updates the 'explored_designs' file if and only if the __gen__ parameter in question is unique,
-        i.e., it cannot be found in the 'explored_designs' file. Note that the __S11__ parameter is only
-        filled as a list when one wants to update the 'explored_designs' file (to save the S11 response from
-        the given __gen__ parameter), hence the geometry list __gen__ and simulation results __S11__ is combined as
-        one list when the user wants to save.
+        Searches for a match in the file Filename with the provided list List.
 
         Parameters:
         -----------
-        __gen__:                    list
-                                    The individual to save, where the list has the format of
-                                    [[x1_min, x1_max, y1_min, y1_max], ..., [xn_min, xn_max, yn_min, yn_max]] for layer n.
-                                    Default is None.
-        __S11__:                    list
-                                    A list that contains a frequency range and the S11 responses from those frequency
-                                    values, i.e., [freq_range, S11_response]. Default is None.
+        Filename:                   str
+                                    The full path, with the filename and possible extension, of the file
+                                    to do the search on.
+        List:                       list
+                                    The list to compare the data within the file Filename with.
 
         Return:
         -------
-        The function will attempt to find a match of the __gen__ parameter in the 'explored_designs' file. If a match has
-        been found, the associated S11 response list will be returned, else 0 will be returned. -1 is returned when an
-        unexpected error occurred.
+        If a match occurs, the full list, with its simulation results, will be returned in the format of
+        [parameters, [return loss, gain]]
 
         Notes:
         ------
@@ -301,50 +353,25 @@ class Filing:
 
         try:
 
-            # The first element is the chromosome list of an individual (its geometry characteristics) and the second
-            # element is the |S11| response values that was collected from CST Studio Suite of the individual, thus:
-            # [[layer_1, layer_2, layer_3], [frequencies, |S11| responses]]
-
             __temp__ = self.Read(Filename=Filename)
 
             for __index__ in range(len(__temp__)):
 
-                # Proceed when the number of layers between the __gen__ and __temp__[__index__] are the same
-                if len(List) == len(__temp__[__index__][0]):
+                # Proceed when the number of parameters between the List and __temp__[__index__] are the same
+                if len(List[0]) == len(__temp__[__index__][0]):
 
-                    __layer_duplicate__ = [False] * len(List)
-                    __number_of_duplicates__ = [0] * len(List)
+                    __parameter_value_duplicate__ = 0
 
-                    # Go through each layer
+                    # Go through each parameter
                     for __i__ in range(len(__temp__[__index__][0])):
 
-                        # Proceed when the first, second, and third elements are equal to one another between
-                        # the __gen__ and __config__.__File__[index]. As well as the number of geometry components
-                        # between the __gen__ and __config__.__File__[index]
-                        if __temp__[__index__][0][__i__][0] == List[__i__][0] and \
-                                __temp__[__index__][0][__i__][1] == List[__i__][1] and \
-                                __temp__[__index__][0][__i__][2] == List[__i__][2] and \
-                                __temp__[__index__][0][__i__][3][0] == List[__i__][3][0] and \
-                                __temp__[__index__][0][__i__][3][1] == List[__i__][3][1] and \
-                                len(__temp__[__index__][0][__i__][4]) == len(List[__i__][4]):
+                        if __temp__[__index__][0][__i__] == List[0][__i__]:
+                            __parameter_value_duplicate__ += 1
 
-                            # Go through each geometry component
-                            for __j__ in range(len(__temp__[__index__][0][__i__][4])):
-
-                                # If the geometry component is matched with __config__.__File__[index][0][__s__][-1]
-                                # geometry component list, increment the appropriate layer counter
-                                if __temp__[__index__][0][__i__][4].__contains__(List[__i__][4][__j__]):
-                                    __number_of_duplicates__[__i__] += 1
-
-                        # If the value of the geometry component counter is the same as the length of the
-                        # __config__.__File__[index][__s__][-1] list (which is the geometry component list), then
-                        # the number of duplicates are the same. Thus, this __gen__ parameter must be a duplicate
-                        if __number_of_duplicates__[__i__] == len(__temp__[__index__][0][__i__][4]):
-                            __layer_duplicate__[__i__] = True
-
-                    # If a duplicate has been found, return the individual
-                    if sum(__layer_duplicate__) == len(__layer_duplicate__):
-                        return __temp__[__index__][1]
+                    # If a duplicate has been found, return the duplicate list that contains the parameters
+                    # and simulation results
+                    if __parameter_value_duplicate__ == len(List[0]):
+                        return __temp__[__index__]
 
             return False
 
