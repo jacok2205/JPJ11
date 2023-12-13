3679d59b55d84f49875bc0b4c252da204e54d44b
Jakes-Work <jacok2205@gmail.com>
2023-12-13 12:04:25 +0200

    antennaGeometry.py was neatened, where the only method to modify is the CheckBoundary() method.

--- rectangular.py
+++ rectangular.py
@@ -1,5 +1,6 @@
 from AntennaDesign.misc import RectangularMicrostripPatch, MicrostripTransmissionLine
 from AntennaDesign.filing import Filing
+from AntennaDesign.antennaGeometry import ModelGeometry
 
 if __name__ == '__main__':
     # Conventional antenna design dimensions
@@ -7,13 +8,15 @@ if __name__ == '__main__':
     RMSPDimensions = RMSP.get_rmsp_dimensions(__sub_h__=0.5e-3)
     MicrostripTransmissionLine.get_microstrip_transmission_dimensions(__freq__=2.4e9, __er__=3.55,
                                                                       __sub_h__=0.5e-3, __Zo__=50)
-    filing = Filing(Directories=None, Debugging=True)
+    # filing = Filing(Directories=None, Debugging=True)
+    #
+    # filing.CreateDirectories(Directories=['\\root'])
+    # filing.CreateFile(Filename='\\root\\test1')
+    # filing.CreateFile(Filename='\\root\\test2')
+    # filing.DeleteFile(Filename='\\root\\test2')
+    #
+    # lists = [[[1, 2, 3], []], [[4, 5, 6], []], [[7, 8, 9], []]]
+    # filing.Save(Filename='\\root\\test1', Lists=lists)
+    # print(filing.Duplicate(Filename='\\root\\test1', List=[[1, 2, 3], []]))
 
-    filing.CreateDirectories(Directories=['\\root'])
-    filing.CreateFile(Filename='\\root\\test1')
-    filing.CreateFile(Filename='\\root\\test2')
-    filing.DeleteFile(Filename='\\root\\test2')
 
-    lists = [[[1, 2, 3], []], [[4, 5, 6], []], [[7, 8, 9], []]]
-    filing.Save(Filename='\\root\\test1', Lists=lists)
-    print(filing.Duplicate(Filename='\\root\\test1', List=[[1, 2, 3], []]))
